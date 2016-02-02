fa167f73fc7e0ac34d06f006f54394b224ce5786
Jeffrey Liu <jeffreyliu8@gmail.com>
2016-02-02 11:10:11 -0800

    first commit

---
+++ app/src/main/java/javis/customview/DefaultActivity.java
@@ -0,0 +1,68 @@
+package javis.customview;
+
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+
+public class DefaultActivity extends AppCompatActivity {
+    PercentView circle1;
+    PercentView circle2;
+    PercentView circle3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_default);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        circle1 = (PercentView) findViewById(R.id.circle1);
+        circle1.setPercentage(2, 4, "of 4 days");
+
+        circle2 = (PercentView) findViewById(R.id.circle2);
+        circle2.setPercentage(29, 30, "of 30 min");
+        circle3 = (PercentView) findViewById(R.id.circle3);
+        circle3.setPercentage(15, 16, "of 16 days");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        animatePercentageView(circle1);
+        animatePercentageView(circle2);
+        animatePercentageView(circle3);
+    }
+
+    public void animatePercentageView(final PercentView percentView) {
+        int numerator = percentView.getNumerator();
+        int denominator = percentView.getDenominator();
+        float percentage = (float) numerator / (float) denominator;
+
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, percentage);
+        valueAnimator.setDuration(2000);
+        valueAnimator.setInterpolator(new DecelerateInterpolator());
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float number = Float.parseFloat(valueAnimator.getAnimatedValue().toString());
+                percentView.setFloatPercentage(number);
+
+            }
+        });
+        valueAnimator.start();
+    }
+}
---
+++ app/src/main/java/javis/customview/PercentView.java
@@ -0,0 +1,189 @@
+package javis.customview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by jeffliu on 1/20/16.
+ */
+public class PercentView extends View {
+    private static final String TAG = "PercentView";
+
+    public PercentView(Context context) {
+        super(context);
+        init();
+    }
+
+    public PercentView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+
+//        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.PercentView, 0, 0);
+//        try {
+//            color = ta.getColor(R.styleable.PercentView, 100.0f);
+//        } finally {
+//            ta.recycle();
+//        }
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.PercentView,
+                0, 0);
+
+        try {
+            ringColor = a.getColor(R.styleable.PercentView_ringColor, Color.BLACK);
+            numerator = a.getInt(R.styleable.PercentView_numerator, 0);
+            denominator = a.getInt(R.styleable.PercentView_denominator, 1);
+            lowerText = a.getString(R.styleable.PercentView_lowerText);
+            upperTextSize = a.getInt(R.styleable.PercentView_upperTextSize, 100);
+            lowerTextSize = a.getInt(R.styleable.PercentView_lowerTextSize, 32);
+            upperYOffset = a.getInt(R.styleable.PercentView_upperYOffset, 20);
+            lowerYOffset = a.getInt(R.styleable.PercentView_lowerYOffset, 50);
+        } finally {
+            a.recycle();
+        }
+    }
+
+    public PercentView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+        rect = new RectF();
+
+        upperTextPaint.setColor(Color.WHITE);
+        upperTextPaint.setTextAlign(Paint.Align.CENTER);
+
+        lowerTextPaint.setColor(Color.WHITE);
+        lowerTextPaint.setTextAlign(Paint.Align.CENTER);
+        lowerTextPaint.setAlpha(ALPHA);
+    }
+
+
+    private RectF rect;
+    private static final float strokeWidth = 16;
+    private int ringColor = Color.BLACK;
+    private int numerator = 0;
+    private float percentage = -1;
+    private int denominator = 1;
+    private String lowerText = "";
+    private int upperTextSize = 100;
+    private int lowerTextSize = 50;
+    private int upperYOffset = 20;
+    private int lowerYOffset = 50;
+    private static final int ALPHA = 153; // 255
+    private static final int ALPHA_RING = 51; // 255
+
+
+    private Paint upperTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Paint lowerTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private final Paint mArcPaint = new Paint() {
+        {
+            setDither(true);
+            setStyle(Paint.Style.STROKE);
+            setStrokeCap(Paint.Cap.ROUND);
+            setStrokeJoin(Paint.Join.ROUND);
+            setColor(ContextCompat.getColor(getContext(), android.R.color.holo_blue_bright));
+            setAlpha(ALPHA_RING);
+            setStrokeWidth(strokeWidth);
+            setAntiAlias(true);
+        }
+    };
+
+    private final Paint mArcPaintFill = new Paint() {
+        {
+            setDither(true);
+            setStyle(Paint.Style.STROKE);
+            setStrokeCap(Paint.Cap.ROUND);
+            setStrokeJoin(Paint.Join.ROUND);
+            setColor(ContextCompat.getColor(getContext(), android.R.color.black));
+            setStrokeWidth(strokeWidth);
+            setAntiAlias(true);
+        }
+    };
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        int size = width > height ? height : width;
+        setMeasuredDimension(size, size);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //draw background circle anyway
+        int left = 0;
+        int width = getWidth();
+        int top = 0;
+
+        float strockWidth = strokeWidth;
+
+        rect.set(left + strockWidth, top + strockWidth, left + width - strockWidth, top + width - strockWidth);
+        mArcPaint.setColor(ringColor);
+        mArcPaint.setAlpha(ALPHA_RING);
+        mArcPaintFill.setColor(ringColor);
+        canvas.drawArc(rect, 0, 360, false, mArcPaint);
+
+        float sweepAngle;
+        if (percentage != -1) {
+            sweepAngle = percentage * 360;
+//            if (percentage == ((float) numerator / (float) denominator)) {
+//                percentage = -1;
+//            }
+        } else {
+            sweepAngle = (float) numerator / (float) denominator * 360;
+        }
+        canvas.drawArc(rect, -90, sweepAngle, false, mArcPaintFill);
+
+        int xTopTextPos = canvas.getWidth() / 2;
+        int yTopTextPos = (int) (((top + width) / 2) - ((upperTextPaint.descent() + upperTextPaint.ascent()) / 2)) - upperYOffset;
+        //((textPaint.descent() + textPaint.ascent()) / 2) is the distance from the baseline to the center.
+        upperTextPaint.setTextSize(upperTextSize);
+        canvas.drawText(Integer.toString(numerator), xTopTextPos, yTopTextPos, upperTextPaint);
+
+        int xBottomTextPos = canvas.getWidth() / 2;
+        int yBotoomTextPos = (int) (((top + width) / 2) - ((lowerTextPaint.descent() + lowerTextPaint.ascent()) / 2)) + lowerYOffset;
+        //((textPaint.descent() + textPaint.ascent()) / 2) is the distance from the baseline to the center.
+        lowerTextPaint.setTextSize(lowerTextSize);
+        canvas.drawText(lowerText, xBottomTextPos, yBotoomTextPos, lowerTextPaint);
+    }
+
+    public void setPercentage(int numerator, int denominator, String lowerText) {
+        if (numerator < 0 || denominator <= 0 || numerator > denominator) {
+            Log.e(TAG, "setPercentage: invalid input");
+            return;
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+        this.lowerText = lowerText;
+        invalidate();
+    }
+
+    public void setFloatPercentage(float percentage) { // 0 to 1
+        if (percentage < 0 || percentage > 1) {
+            Log.e(TAG, "setPercentage: invalid input");
+            return;
+        }
+        this.percentage = percentage;
+        invalidate();
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+}
